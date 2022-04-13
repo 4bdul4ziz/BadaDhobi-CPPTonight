337537cd9cfc443c639d240c08028c55ea5af9fc
Abdul Aziz A.B <66479632+4bdul4ziz@users.noreply.github.com>
2022-04-13 14:18:31 +0530

    Updates

---
+++ precache-manifest.4a276e405e14c0bd3ab62ad841250387.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26eed0d01ab9a2a71a95dc45834517e7",
+    "url": "/CPP-Tonight/index.html"
+  },
+  {
+    "revision": "55209b272e0ac384124e",
+    "url": "/CPP-Tonight/static/css/2.2078a296.chunk.css"
+  },
+  {
+    "revision": "e93e01701183516b03aa",
+    "url": "/CPP-Tonight/static/css/main.0c9b6d16.chunk.css"
+  },
+  {
+    "revision": "55209b272e0ac384124e",
+    "url": "/CPP-Tonight/static/js/2.ed8fbf7f.chunk.js"
+  },
+  {
+    "revision": "6a012143643a23eb000cc130bf1d1459",
+    "url": "/CPP-Tonight/static/js/2.ed8fbf7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e93e01701183516b03aa",
+    "url": "/CPP-Tonight/static/js/main.147cf579.chunk.js"
+  },
+  {
+    "revision": "20250b43733e05e39b23",
+    "url": "/CPP-Tonight/static/js/runtime-main.58f5aa02.js"
+  },
+  {
+    "revision": "f99fa1b25b111dd619b0889d6ccba797",
+    "url": "/CPP-Tonight/static/media/BG-Order.f99fa1b2.png"
+  },
+  {
+    "revision": "ed17afdecab0499abd5be9f5be57a97a",
+    "url": "/CPP-Tonight/static/media/Preloader.ed17afde.gif"
+  },
+  {
+    "revision": "270f72df47e036d9cfa11f91f0a94a60",
+    "url": "/CPP-Tonight/static/media/arrow_left.270f72df.svg"
+  },
+  {
+    "revision": "d9ccc77c8240ad0dc2724cf333c0479e",
+    "url": "/CPP-Tonight/static/media/arrow_right.d9ccc77c.svg"
+  },
+  {
+    "revision": "0b96d866c6029786f3f97210d7664633",
+    "url": "/CPP-Tonight/static/media/biography-bg.0b96d866.png"
+  },
+  {
+    "revision": "2181b1d3ed38ce1d5cfdf82ea9d7daed",
+    "url": "/CPP-Tonight/static/media/customer-1.2181b1d3.jpg"
+  },
+  {
+    "revision": "c2181e501fea77a2ad45c9d98aceb175",
+    "url": "/CPP-Tonight/static/media/customer-2.c2181e50.jpg"
+  },
+  {
+    "revision": "e0d2b4110a0449ed38c96125ca8e6123",
+    "url": "/CPP-Tonight/static/media/customer-3.e0d2b411.jpg"
+  },
+  {
+    "revision": "6a5bef2e071dcf77c694cf35dbd6065d",
+    "url": "/CPP-Tonight/static/media/dry-cleaning.6a5bef2e.webp"
+  },
+  {
+    "revision": "2f12242375edd68e9013ecfb59c672e9",
+    "url": "/CPP-Tonight/static/media/fa-brands-400.2f122423.svg"
+  },
+  {
+    "revision": "8300bd7f30e0a313c1d772b49d96cb8e",
+    "url": "/CPP-Tonight/static/media/fa-brands-400.8300bd7f.ttf"
+  },
+  {
+    "revision": "ad527cc5ec23d6da66e8a1d6772ea6d3",
+    "url": "/CPP-Tonight/static/media/fa-brands-400.ad527cc5.woff"
+  },
+  {
+    "revision": "e2ca6541bff3a3e9f4799ee327b28c58",
+    "url": "/CPP-Tonight/static/media/fa-brands-400.e2ca6541.eot"
+  },
+  {
+    "revision": "f075c50f89795e4cdb4d45b51f1a6800",
+    "url": "/CPP-Tonight/static/media/fa-brands-400.f075c50f.woff2"
+  },
+  {
+    "revision": "3602b7e8b2cb1462b0bef9738757ef8a",
+    "url": "/CPP-Tonight/static/media/fa-regular-400.3602b7e8.svg"
+  },
+  {
+    "revision": "3c6879c4f342203d099bdd66dce6d396",
+    "url": "/CPP-Tonight/static/media/fa-regular-400.3c6879c4.woff"
+  },
+  {
+    "revision": "49f00693b0e5d45097832ef5ea1bc541",
+    "url": "/CPP-Tonight/static/media/fa-regular-400.49f00693.ttf"
+  },
+  {
+    "revision": "4a74738e7728e93c4394b8604081da62",
+    "url": "/CPP-Tonight/static/media/fa-regular-400.4a74738e.woff2"
+  },
+  {
+    "revision": "b01516c1808be557667befec76cd6318",
+    "url": "/CPP-Tonight/static/media/fa-regular-400.b01516c1.eot"
+  },
+  {
+    "revision": "205f07b3883c484f27f40d21a92950d4",
+    "url": "/CPP-Tonight/static/media/fa-solid-900.205f07b3.ttf"
+  },
+  {
+    "revision": "4451e1d86df7491dd874f2c41eee1053",
+    "url": "/CPP-Tonight/static/media/fa-solid-900.4451e1d8.woff"
+  },
+  {
+    "revision": "664de3932dd6291b4b8a8c0ddbcb4c61",
+    "url": "/CPP-Tonight/static/media/fa-solid-900.664de393.svg"
+  },
+  {
+    "revision": "8ac3167427b1d5d2967646bd8f7a0587",
+    "url": "/CPP-Tonight/static/media/fa-solid-900.8ac31674.eot"
+  },
+  {
+    "revision": "8e1ed89b6ccb8ce41faf5cb672677105",
+    "url": "/CPP-Tonight/static/media/fa-solid-900.8e1ed89b.woff2"
+  },
+  {
+    "revision": "14137f89247c1ab0eb29e8b75ef6e9d6",
+    "url": "/CPP-Tonight/static/media/google.14137f89.svg"
+  },
+  {
+    "revision": "088a255232a7d06b3a329feb9cebe5a4",
+    "url": "/CPP-Tonight/static/media/icon.088a2552.svg"
+  },
+  {
+    "revision": "f65eb568cec30fdcdf17e4f55ceccc11",
+    "url": "/CPP-Tonight/static/media/log.f65eb568.png"
+  },
+  {
+    "revision": "1763cc1f15df95fa57cf29d3492b161b",
+    "url": "/CPP-Tonight/static/media/logo.1763cc1f.png"
+  },
+  {
+    "revision": "0c80f2e277a1c88a779c40f807a2781b",
+    "url": "/CPP-Tonight/static/media/metamask.0c80f2e2.webp"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/CPP-Tonight/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/CPP-Tonight/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/CPP-Tonight/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/CPP-Tonight/static/media/nucleo-outline.8ebec31f.woff2"
+  },
+  {
+    "revision": "80af570fb77416d3db3651da4d51e82d",
+    "url": "/CPP-Tonight/static/media/register.80af570f.svg"
+  },
+  {
+    "revision": "3ba05dd03a4f890ea07e0c9484d8add6",
+    "url": "/CPP-Tonight/static/media/subscription.3ba05dd0.jpg"
+  },
+  {
+    "revision": "1763cc1f15df95fa57cf29d3492b161b",
+    "url": "/CPP-Tonight/static/media/videoImg.1763cc1f.jpg"
+  },
+  {
+    "revision": "a47c473a3b3a9d7efd45459fea4976a2",
+    "url": "/CPP-Tonight/static/media/wash-iron.a47c473a.jpeg"
+  },
+  {
+    "revision": "952e6f35b2f839c7116a62e8d9dbde34",
+    "url": "/CPP-Tonight/static/media/wash.952e6f35.png"
+  },
+  {
+    "revision": "a48fffa43b54f78bfd5724df41f61b2a",
+    "url": "/CPP-Tonight/static/media/wash2.a48fffa4.png"
+  },
+  {
+    "revision": "744e36820f090e5c3eb0cccc9da58700",
+    "url": "/CPP-Tonight/static/media/works.744e3682.mp4"
+  }
+]);
